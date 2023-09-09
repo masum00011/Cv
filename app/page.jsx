907beaa0aaa85cada758753701b35687a604f95c
Masum Reza <masum.r@antino.io>
2023-09-09 12:20:39 +0530

    add routes

--- app/page.jsx
+++ app/page.jsx
@@ -2,18 +2,19 @@ import Image from "next/image";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import bgcover from "./assets/images/cover.jpg";
+import Link from "next/link";
 // import ReactJS_Developer from "./assets/images/ReactJS_Developer.pdf";
 // import cv from '../public/'
-import Link from "next/link";
 export default function Home() {
+     
   return (
     <main className="container m-auto h-screen">
       <section className="Navbar flex justify-between  py-4 items-center">
         <h1 className="text-[1.5rem] text-[blue]">Portfolio.</h1>
         <div className="text flex gap-4">
           <p>Home</p>
-          <p>About</p>
-          <p>Skill</p>
+          <Link href="/about">About Us</Link>
+          <Link href="/skill">Skill</Link>
         </div>
         <div className="social-icon flex gap-4 text-[blue]">
           <a href="https://www.linkedin.com/in/masum-reza-148397199/">
@@ -33,7 +34,7 @@ export default function Home() {
             <b className="text-[4rem]">I'm Masum Reza</b>
           </div>
           <div>
-            <b className="text-[3rem] text-[blue]">ReactJS/NextJS Developer</b>
+            <b className="text-[3rem] text-[blue] break-all">ReactJS/NextJS Developer</b>
           </div>
           <div>
             <p>From Gurgaon</p>
